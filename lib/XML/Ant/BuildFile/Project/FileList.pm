6d9c31a11dd3af62474cc0c737926803dfb508d7
Mark Gardner <mjgardner@cpan.org>
2011-03-23 01:47:07 +0000

    Import of MJGARDNER/XML-Ant-BuildFile-0.201 from CPAN.
    gitpan-cpan-distribution: XML-Ant-BuildFile
    gitpan-cpan-version:      0.201
    gitpan-cpan-path:         MJGARDNER/XML-Ant-BuildFile-0.201.tar.gz
    gitpan-cpan-author:       MJGARDNER
    gitpan-cpan-maturity:     released

--- lib/XML/Ant/BuildFile/Project/FileList.pm
+++ lib/XML/Ant/BuildFile/Project/FileList.pm
@@ -12,7 +12,7 @@ use Modern::Perl;    ## no critic (UselessNoCritic,RequireExplicitPackage)
 package XML::Ant::BuildFile::Project::FileList;
 
 BEGIN {
-    $XML::Ant::BuildFile::Project::FileList::VERSION = '0.200';
+    $XML::Ant::BuildFile::Project::FileList::VERSION = '0.201';
 }
 
 # ABSTRACT: file list node within an Ant build file
@@ -109,7 +109,7 @@ XML::Ant::BuildFile::Project::FileList - file list node within an Ant build file
 
 =head1 VERSION
 
-version 0.200
+version 0.201
 
 =head1 SYNOPSIS
 
